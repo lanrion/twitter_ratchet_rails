e479981eb058fb08d26fe58c0fd781a10f609c84
lanrion <huaitao-deng@foxmail.com>
2014-06-20 20:22:50 +0800

    added test app

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :users
+
+  root "users#index"
+
   mount TwitterRatchetRails::Engine => "/twitter_ratchet_rails"
 end
--- test/twitter_ratchet_rails_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TwitterRatchetRailsTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, TwitterRatchetRails
-  end
-end
